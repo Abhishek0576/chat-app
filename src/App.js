824cd6b4d068d27ee587d1ec3b9412ce1cc4cf40
Abhishek0576 <69306615+Abhishek0576@users.noreply.github.com>
2021-10-03 19:08:23 +0530

    Modified

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {ChatEngine} from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="
+            418f00f6-fcda-4b67-a960-e06f209b4f86"   
+            userName="abkstar"
+            userSecret="123456"
+        />
+    )
+}
+
+export default App;
