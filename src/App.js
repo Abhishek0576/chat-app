cd93dd47d260ef7e2dfcc86085188a43968bc71e
Abhishek0576 <69306615+Abhishek0576@users.noreply.github.com>
2021-10-03 19:32:27 +0530

    App.js Modified

--- src/App.js
+++ src/App.js
@@ -5,16 +5,10 @@ const App = () => {
     return(
         <ChatEngine
             height="100vh"
-<<<<<<< HEAD
             projectID="
             418f00f6-fcda-4b67-a960-e06f209b4f86"   
             userName="abkstar"
             userSecret="123456"
-=======
-            projectID=""
-            userName=""
-            userSecret=""
->>>>>>> d15eb40fe644ec37965631217431a19c59a741a0
         />
     )
 }
