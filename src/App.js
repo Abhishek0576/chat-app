30824d4e32992dfed5d263f2916217759d337234
Abhishek0576 <69306615+Abhishek0576@users.noreply.github.com>
2021-10-03 19:42:06 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import {ChatEngine} from 'react-chat-engine';
 import './App.css';
 
+import { ChatFeed } from './components/ChatFeed';
+
 const App = () => {
     return(
         <ChatEngine
             height="100vh"
-            projectID="
-            418f00f6-fcda-4b67-a960-e06f209b4f86"   
+            projectID="418f00f6-fcda-4b67-a960-e06f209b4f86"   
             userName="abkstar"
             userSecret="123456"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
         />
     )
 }
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ChatFeed = (props) => {
+    console.log(props);
+
+    return(
+        <div>
+            ChatFeed
+        </div>
+    )
+}
+
+export default ChatFeed;
